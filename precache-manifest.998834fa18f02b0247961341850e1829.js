6760217adb03d481ebc4fb51d46fb633dd8d673d
Hammad Balkhi <syedbalkhi@Geotab.local>
2019-07-12 14:10:18 -0400

    Updates

---
+++ precache-manifest.998834fa18f02b0247961341850e1829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34480c1a21f40ec4d18",
+    "url": "/Dad-Jokes/static/js/runtime~main.a34480c1.js"
+  },
+  {
+    "revision": "89631608fdeffaae6f03",
+    "url": "/Dad-Jokes/static/js/main.89631608.chunk.js"
+  },
+  {
+    "revision": "34ec86ec11e3d6c4b4d0",
+    "url": "/Dad-Jokes/static/js/1.34ec86ec.chunk.js"
+  },
+  {
+    "revision": "89631608fdeffaae6f03",
+    "url": "/Dad-Jokes/static/css/main.9b333b22.chunk.css"
+  },
+  {
+    "revision": "214b70b0934ab44dfe4026b5a2083290",
+    "url": "/Dad-Jokes/index.html"
+  }
+];
